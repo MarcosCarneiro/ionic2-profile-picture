26bb5d85275d3d2fd7e2d6be5aa7052389537751
Marcos Carneiro Lima <marcos.lima.crn@gmail.com>
2017-01-07 16:03:49 -0300

    feat(home): Gera a URL Gravatar para acessar a imagem do perfil referente ao email digitado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import md5 from 'crypto-md5';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  email: any;
+  password: any;
+  profilePicture: any = "https://www.gravatar.com/avatar/";
+
   constructor(public navCtrl: NavController) {
-    
+
+  }
+
+  emailChanged(){
+    this.profilePicture = "https://www.gravatar.com/avatar/" + md5(this.email.toLowerCase(), 'hex')
   }
 
 }
